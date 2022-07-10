6a13bc84c93dced9d88da6049117860b302388e2
Seungwoo Lee <timosean@naver.com>
2022-07-10 23:21:52 +0900

    style: add layout

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Header from '../components/Header';
+
+type AppLayoutProps = {
+    children: React.ReactNode;
+};
+
+function Layout({ children }: AppLayoutProps) {
+    return (
+        <div>
+            <Head>
+                <title>NERF</title>
+            </Head>
+            <Header />
+            <div>{children}</div>
+        </div>
+    );
+}
+
+export default Layout;
