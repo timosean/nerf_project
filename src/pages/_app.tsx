3ab48722cb0fee9fa96392483885b92014aae494
Seungwoo Lee <timosean@naver.com>
2022-07-10 23:29:29 +0900

    fix: change import directories in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css';
+import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
-import Layout from '../layouts/layout';
+import Layout from '../layouts/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
