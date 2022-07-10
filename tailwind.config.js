f0ff517b5c6e0996ffdf6f59dadfd15618d5276b
Seungwoo Lee <timosean@naver.com>
2022-07-10 23:24:37 +0900

    feat: add font styles, color styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                ourBlack: '#1D1D1D',
+                outWhite: '#F8F8F8',
+            },
+        },
+    },
+    plugins: [],
+};
