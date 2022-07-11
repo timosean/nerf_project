6e616cc377ae27ab7a44d83532e58d6c9a542d32
Seungwoo Lee <timosean@naver.com>
2022-07-11 12:50:05 +0900

    style: change mobile header menu state name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import { useState } from 'react';
 
 function Header() {
-    const [showSidebar, setShowSidebar] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
 
     return (
         <header className="relative p-[6vw] z-10 w-full md:py-[2.5vw] md:px-[3vw]">
@@ -12,16 +12,18 @@ function Header() {
                 </Link>
 
                 {/* Mobile Sidebar Menu Icon (Burger Box) */}
-                <button className="w-[35px] h-[35px] relative flex flex-col justify-center items-center px-[6px] box-content">
+                <button className="w-[35px] h-[35px] relative flex flex-col justify-center items-center px-[6px] box-content md:hidden">
                     <div
                         className={`relative flex justify-center items-center w-full h-[1px] transition-all before:content-[' '] before:absolute before:w-full before:h-[1px] before:bg-ourBlack before:transition-all before:transition-all before:duration-[250ms] before:ease-[cubic-bezier(.2,.6,.3,1)] after:content-[' '] after:absolute after:w-full after:h-[1px] after:bg-ourBlack after:transition-all after:transition-all after:duration-[250ms] after:ease-[cubic-bezier(.2,.6,.3,1)] ${
-                            showSidebar
+                            showMenu
                                 ? 'before:rotate-[-135deg] before:translate-x-[5.5px] before:w-[28px] after:rotate-[135deg] after:translate-x-[5.5px] after:w-[28px]'
                                 : 'before:translate-y-[-5.5px] after:translate-y-[5.5px]'
                         }`}
-                        onClick={() => setShowSidebar(!showSidebar)}
+                        onClick={() => setShowMenu(!showMenu)}
                     ></div>
                 </button>
+
+                {/* Mobile Header Menu Area */}
             </div>
         </header>
     );
