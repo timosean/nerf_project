1cdbb822921a0a27357756c6f3899dbc68b8ab4a
Seungwoo Lee <timosean@naver.com>
2022-07-17 22:17:42 +0900

    feat: add cursor-pointer class to placelist items

--- src/components/PlaceList.tsx
+++ src/components/PlaceList.tsx
@@ -12,7 +12,7 @@ function PlaceList({}) {
         <div className="relative grid gap-3 grid-cols-1 py-[3vw] md:grid-cols-2">
             {placeArr.map((place, idx) => (
                 <Link href={`/place/${place}`}>
-                    <div key={`${idx}. ${place}`} className="relative" onMouseOver={() => setIsHover(idx)} onMouseOut={() => setIsHover(100)}>
+                    <div key={`${idx}. ${place}`} className="relative cursor-pointer" onMouseOver={() => setIsHover(idx)} onMouseOut={() => setIsHover(100)}>
                         <img src="/img_example.jpg" className={`${isHover == idx ? `opacity-30` : `opacity-100`} transition duration-1000`} alt={`${place}`} />
                         <a className={`${isHover == idx ? 'opacity-100 visible translate-x-6' : 'opacity-0'} transition duration-1000 text-lg font-[700] bottom-14 z-20 absolute`}>{place}</a>
                         <div className={`${isHover == idx ? 'opacity-100 visible translate-x-6' : 'opacity-0'} transition duration-1000 absolute z-20 bottom-6 flex space-x-4`}>
