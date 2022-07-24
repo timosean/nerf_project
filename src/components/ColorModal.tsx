83c9ee8840c7ed2b513700ae9873c10314d87a58
Seungwoo Lee <timosean@naver.com>
2022-07-24 17:03:24 +0900

    feat: add close modal without close button, add backdrop blur

--- src/components/ColorModal.tsx
+++ src/components/ColorModal.tsx
@@ -1,12 +1,21 @@
 import { MdOutlineClose } from 'react-icons/md';
+import { modalState } from '../states/index';
+import { useRecoilState } from 'recoil';
 
-function ColorModal({ color }: { color: string }) {
+function ColorModal({ color, isOpen }: { color: string; isOpen: boolean }) {
+    const [modal, setModal] = useRecoilState(modalState);
+
     return (
-        <div className="absolute inset-0 bg-[rgba(0,0,0,0.4)] flex justify-center items-center z-30 px-[6vw] md:px-[3vw]">
-            <div className="w-full h-[43rem] bg-white p-4 rounded-xl mt-[5rem]">
+        <>
+            <div className={`${modal ? 'opacity-100 flex' : 'opacity-0 hidden'} transition-all absolute inset-0 bg-[rgba(0,0,0,0.4)] z-30 px-[6vw] md:px-[3vw]`} onClick={() => setModal(false)}></div>
+            <div
+                className={`${
+                    modal ? 'opacity-100 flex' : 'opacity-0 hidden'
+                } absolute top-[55%] left-[50%] -translate-x-[50%] -translate-y-[50%] w-5/6 lg:w-4/6 h-[43rem] bg-[rgba(229,231,235,0.4)] backdrop-blur-3xl p-4 rounded-xl z-40`}
+            >
                 <h1>{color}</h1>
             </div>
-        </div>
+        </>
     );
 }
 
