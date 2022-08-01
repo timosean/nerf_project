1059cff9c6b6c5c9326a4634ca5b3c85f8bb8b6b
Seungwoo Lee <timosean@naver.com>
2022-08-01 11:24:57 +0900

    feat: add fadein effect to intropage

--- src/pages/place/[place].tsx
+++ src/pages/place/[place].tsx
@@ -4,6 +4,7 @@ import dataList from '../../constants/dataList';
 import { modalState, colorState } from '../../states/index';
 import { useRecoilState } from 'recoil';
 import ColorModal from '../../components/ColorModal';
+import { useEffect } from 'react';
 
 //원래 높이는 h-[calc(100vh-5vw-2.5rem)] 이거였음.
 
@@ -47,6 +48,15 @@ function Place() {
     const [modal, setModal] = useRecoilState(modalState);
     const [color, setColor] = useRecoilState(colorState);
 
+    useEffect(() => {
+        function introFadeIn() {
+            const introSection = document.querySelector('#intro-section') as HTMLElement;
+            introSection.classList.replace('opacity-0', 'opacity-100');
+        }
+
+        introFadeIn();
+    }, []);
+
     // dataList 배열에서 이름 검사하는 함수
     const isPlace = (element: DataType) => {
         return element.place === place?.toString();
@@ -55,14 +65,14 @@ function Place() {
     // dataList에서 이름이 같은 element를 data에 할당
     const data: DataType | undefined = dataList.find(isPlace);
 
+    //undefined, null 등을 체크
     if (!data) {
-        //undefined, null 등을 체크
         return null;
     }
 
     return (
         <div className="flex flex-col justify-center items-center">
-            <section className="w-full h-full md:h-[calc(100vh-5vw-2.5rem)]">
+            <section className="w-full h-full md:h-[calc(100vh-5vw-2.5rem)] opacity-0 duration-1000" id="intro-section">
                 <Introduce placename={place?.toString()} />
             </section>
             <section className="w-full h-full md:h-[100vh] mt-24 md:static flex items-center justify-center">
