7c4dfd534fc3a0b50269474519f0847387470027
spblue4422 <spblue4422@naver.com>
2022-08-09 23:47:11 +0900

    change modal color

--- src/components/ColorModal.tsx
+++ src/components/ColorModal.tsx
@@ -5,6 +5,16 @@ import { useRecoilState } from 'recoil';
 function ColorModal({ color }: { color: string }) {
     const [modal, setModal] = useRecoilState(modalState);
 
+    //바꾸고 싶으면 대충 이 함수 손봐서 넣으면 됨. 색에 opacity가 들어가야 blur처리가 되나봄.
+    function hexToRgb(color: string, opac:string) {
+        const hex: string = color.replace('#', '');
+        const r = parseInt(hex.substring(0, 2), 16).toString();
+        const g = parseInt(hex.substring(2, 4), 16).toString();
+        const b = parseInt(hex.substring(4, 6), 16).toString();
+
+        return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + opac + ')'
+    }
+    
     function onModalClose() {
         const modalBackground = document.querySelector('#modal-background') as HTMLDivElement;
         const modal = document.querySelector('#modal') as HTMLDivElement;
@@ -22,8 +32,8 @@ function ColorModal({ color }: { color: string }) {
     return (
         <>
             <div
-                className={`opacity-0 top-full transition-opacity duration-500 backdrop-blur absolute left-0 right-0 bg-[rgba(100,100,100,0.7)] z-30 px-[6vw] md:px-[3vw]`}
-                // style={{backgroundColor: `${color}`}}
+                className={`opacity-0 top-full transition-opacity backdrop-blur duration-500 absolute left-0 right-0 z-30 px-[6vw] md:px-[3vw]`}
+                style={{backgroundColor: `${hexToRgb(color, '0.7')}`}}
                 onClick={() => {
                     onModalClose();
                     setModal(false);
@@ -33,10 +43,10 @@ function ColorModal({ color }: { color: string }) {
             <div
                 id="modal"
                 className={`opacity-0 -translate-y-[1000%] transition-opacity duration-500 absolute top-[55%] left-[50%] -translate-x-[50%] flex flex-col w-5/6 lg:w-4/6 h-[30rem] backdrop-blur-3xl p-6 rounded-xl z-40`}
-                style={{backgroundColor: `${color}`}}
+                style={{backgroundColor: `${hexToRgb(color, '0.7')}`}}
             >
                 <h1 className="text-3xl lg:text-5xl font-bold mb-6 text-white">{color}</h1>
-                <p className="overflow-auto">text</p>
+                <p className="overflow-auto">${hexToRgb(color, '0.7')}</p>
             </div>
         </>
     );
